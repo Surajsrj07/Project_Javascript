67baac3b2be23ada02a58a54b85ca190b08210ab
Surajsrj07 <Surajsrj93@gmail.com>
2022-11-17 23:42:07 +0530

    js

---
+++ 04_Stop Clock/script.js
@@ -0,0 +1,62 @@
+const btnStart = document.querySelector('.start');
+const btnStop = document.querySelector('.stop');
+const btnReset = document.querySelector('.reset');
+
+let hrs = mins = secs = ms = 0, startTimer;
+
+btnStart.addEventListener('click', () => {
+
+startTimer =setInterval(() => {
+ 
+    ms++; //ms=ms+1
+    if(ms==100){
+        secs++;
+        ms=0;
+    }
+    if(secs==60){
+        mins++;
+        secs=0;
+    }
+    if(mins==60){
+        hrs++;
+        mins=0;
+    }
+   updateDisplay();
+    
+}, 10);
+
+
+});
+
+
+btnStop.addEventListener('click', () => {
+    clearInterval(startTimer);
+
+});
+
+
+btnReset.addEventListener('click', () => {
+    hrs = mins = secs = ms = 0;
+    clearInterval(startTimer);
+
+    updateDisplay();
+});
+
+
+
+function updateDisplay() {
+
+    //Formatted Display
+
+    phrs = hrs < 10 ? '0' + hrs : hrs;
+    pmins = mins < 10 ? '0' + mins : mins;
+    psecs = secs < 10 ? '0' + secs : secs;
+    pms = ms < 10 ? '0' + ms : ms;
+
+    //Output Display
+    document.querySelector('.hrs').innerText = phrs;
+    document.querySelector('.mins').innerText = pmins;
+    document.querySelector('.secs').innerText = psecs;
+    document.querySelector('.ms').innerText = pms;
+
+}
