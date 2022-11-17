3cfc3a7eb5efff593c4f938866b4493da17eeeda
Surajsrj07 <Surajsrj93@gmail.com>
2022-11-17 23:59:07 +0530

    js part

--- 04_Stop Clock/script.js
+++ 04_Stop Clock/script.js
@@ -4,32 +4,39 @@ const btnReset = document.querySelector('.reset');
 
 let hrs = mins = secs = ms = 0, startTimer;
 
+
+btnStart.classList.add('start-active');
+btnStop.classList.remove('stop-active');
+
 btnStart.addEventListener('click', () => {
 
-startTimer =setInterval(() => {
- 
-    ms++; //ms=ms+1
-    if(ms==100){
-        secs++;
-        ms=0;
-    }
-    if(secs==60){
-        mins++;
-        secs=0;
-    }
-    if(mins==60){
-        hrs++;
-        mins=0;
-    }
-   updateDisplay();
-    
-}, 10);
+    startTimer = setInterval(() => {
+
+        ms++; //ms=ms+1
+        if (ms == 100) {
+            secs++;
+            ms = 0;
+        }
+        if (secs == 60) {
+            mins++;
+            secs = 0;
+        }
+        if (mins == 60) {
+            hrs++;
+            mins = 0;
+        }
+        updateDisplay();
+
+    }, 10);
 
 
 });
 
 
 btnStop.addEventListener('click', () => {
+    btnStart.classList.remove('start-active');
+    btnStop.classList.add('stop-active');
+
     clearInterval(startTimer);
 
 });
@@ -38,6 +45,8 @@ btnStop.addEventListener('click', () => {
 btnReset.addEventListener('click', () => {
     hrs = mins = secs = ms = 0;
     clearInterval(startTimer);
+    btnStart.classList.remove('start-active');
+    btnStop.classList.remove('stop-active');
 
     updateDisplay();
 });
